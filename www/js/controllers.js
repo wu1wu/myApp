a336aa73b528cbd9988d4feeb916d3b7ec967cef
wu1wu <szwuxi@gmail.com>
2014-07-30 22:56:53 +0800

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,36 @@
 angular.module('starter.controllers', [])
 
+
+
+
+.controller('LocationCtrl', ['$scope', '$cordovaGeolocation', function($scope, $cordovaGeolocation) {
+
+  var lastPosition = null;
+  $scope.locationError = null;
+  $scope.locationHistoty = [];
+  $scope.counter = 0;
+
+
+  var options = { maximumAge: 10000, timeout: 20000, enableHighAccuracy: true };
+
+  $cordovaGeolocation.watchPosition(options).then(function() {
+      // Not currently used
+    }, function(err) {
+      $scope.locationError = err;
+    }, function(position) {
+      $scope.locationError = null;
+      $scope.counter++;
+      
+      if(!lastPosition || lastPosition.coords.latitude !== position.coords.latitude || lastPosition.coords.longitude !== position.coords.longitude) {
+        lastPosition = position;
+        $scope.locationHistoty.unshift(position);
+      }
+
+  });
+
+}])
+
+
 .controller('DashCtrl', function($scope) {
 })
 
